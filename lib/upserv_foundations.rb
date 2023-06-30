ad20596f07996d00698173cd5d3dde411389ce29
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-06-30 18:43:19 -0500

    Update laoders

--- lib/upserv_foundations.rb
+++ lib/upserv_foundations.rb
@@ -2,13 +2,17 @@
 
 require_relative 'upserv_foundations/version'
 
-# Add all helper files to ActionView::Base
-Dir.glob('lib/upserv_foundations/helpers/**/*.rb').each do |file|
-  require file.sub(%r{^lib/}, '')
-  class_name = file.sub(%r{^lib/}, '').sub(/\.rb$/, '')
-                   .gsub('/', '_').split('_').map(&:capitalize).join
-  ActionView::Base.include Object.const_get(class_name)
-end
+require 'upserv_foundations/helpers/components/ui_helper'
+ActionView::Base.include UpservFoundations::Components::UiHelper
+
+# # Add all helper files to ActionView::Base
+# Dir.glob('lib/upserv_foundations/helpers/**/*.rb').each do |file|
+#   require file.sub('lib/', '').sub('.rb', '')
+#   class_name = file.sub('lib/upserv_foundations/helpers', 'upserv_foundations')
+#                    .sub('.rb', '').split('_').map(&:capitalize).join.gsub('/', '::')
+#                    .split('::').map(&:capitalize).join('::')
+#   ActionView::Base.include Object.const_get(class_name)
+# end
 
 # Add things you would want in all rails apps
 module UpservFoundations
