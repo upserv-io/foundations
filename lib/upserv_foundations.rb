79746ecd7bad98bd6ba53a7d0397d0d83beb6535
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-06-30 18:21:56 -0500

    Fix autoloading of helpers

--- lib/upserv_foundations.rb
+++ lib/upserv_foundations.rb
@@ -4,7 +4,7 @@ require_relative 'upserv_foundations/version'
 
 # Add all helper files to ActionView::Base
 Dir.glob('lib/upserv_foundations/helpers/**/*.rb').each do |file|
-  require file
+  require file.sub(%r{^lib/}, '')
   class_name = file.sub(%r{^lib/}, '').sub(/\.rb$/, '')
                    .gsub('/', '_').split('_').map(&:capitalize).join
   ActionView::Base.include Object.const_get(class_name)
