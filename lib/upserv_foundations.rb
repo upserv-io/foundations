180a7738b32db0c0b4b37834e39171633b3ab189
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-06-30 18:10:21 -0500

    Auto load helpers

--- lib/upserv_foundations.rb
+++ lib/upserv_foundations.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 require_relative 'upserv_foundations/version'
-require 'upserv_foundations/global/components/ui_helper'
-ActionView::Base.include Global::Components::UiHelper
+
+# Add all helper files to ActionView::Base
+Dir.glob('lib/upserv_foundations/helpers/**/*.rb').each do |file|
+  require file
+  class_name = file.sub(%r{^lib/}, '').sub(/\.rb$/, '')
+                   .gsub('/', '_').split('_').map(&:capitalize).join
+  ActionView::Base.include Object.const_get(class_name)
+end
 
 # Add things you would want in all rails apps
 module UpservFoundations
--- lib/upserv_foundations/helpers/components/ui_helper.rb
+++ lib/upserv_foundations/helpers/components/ui_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module Global
+module UpservFoundations
   module Components
     # Set up ui components
     module UiHelper
