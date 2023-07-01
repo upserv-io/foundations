4d371d069a3ab2cccd592f8b75766e27bbfcb925
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-06-30 21:01:03 -0500

    add views

--- lib/upserv_foundations/helpers/components/ui_helper.rb
+++ lib/upserv_foundations/helpers/components/ui_helper.rb
@@ -7,7 +7,17 @@ module Components
       options[:block] = block
       options[:klass] = options[:class]
       # concat(render('components/ui/dropdown', options))
-      concat(content_tag('div', class: 'party'))
+      # concat(content_tag('div', class: 'party'))
+      content_tag(
+        'div',
+        class: "flex justify-center#{" #{options[:class]}" if options[:class]}",
+        data: { controller: 'dropdown',
+                action: 'click@window->dropdown#hide touchend@window->dropdown#hide' }
+      ) do
+        content_tag 'div', class: 'dropdown relative' do
+          block.call
+        end
+      end
     end
 
     def dropdown_button(options = {}, &block)
