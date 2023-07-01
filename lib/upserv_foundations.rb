e493c7125f16183ee56ac12498bc76d2e78d9a3a
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-06-30 19:19:18 -0500

    Fix loaders

--- lib/upserv_foundations.rb
+++ lib/upserv_foundations.rb
@@ -2,8 +2,8 @@
 
 require_relative 'upserv_foundations/version'
 
-require 'upserv_foundations/helpers/components/ui_helper'
-ActionView::Base.include UpservFoundations::Components::UiHelper
+# require 'upserv_foundations/helpers/components/ui_helper'
+# ActionView::Base.include UpservFoundations::Components::UiHelper
 
 # Add all helper files to ActionView::Base
 Dir.glob('lib/upserv_foundations/helpers/**/*.rb').each do |file|
@@ -22,7 +22,7 @@ Dir.glob('lib/upserv_foundations/helpers/**/*.rb').each do |file|
   class_name += "::#{File.basename(file, '.rb').split('_').map(&:capitalize).join}"
   puts  require_file
   puts  class_name
-  # ActionView::Base.include Object.const_get(class_name)
+  ActionView::Base.include Object.const_get(class_name)
 end
 
 # Add things you would want in all rails apps
