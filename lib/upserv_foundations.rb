e226c7807326e6142e15b019b5bc8313ca8f7897
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-06-30 19:00:29 -0500

    Fix loaders

--- lib/upserv_foundations.rb
+++ lib/upserv_foundations.rb
@@ -2,17 +2,19 @@
 
 require_relative 'upserv_foundations/version'
 
-require 'upserv_foundations/helpers/components/ui_helper'
-ActionView::Base.include UpservFoundations::Components::UiHelper
-
-# # Add all helper files to ActionView::Base
-# Dir.glob('lib/upserv_foundations/helpers/**/*.rb').each do |file|
-#   require file.sub('lib/', '').sub('.rb', '')
-#   class_name = file.sub('lib/upserv_foundations/helpers', 'upserv_foundations')
-#                    .sub('.rb', '').split('_').map(&:capitalize).join.gsub('/', '::')
-#                    .split('::').map(&:capitalize).join('::')
-#   ActionView::Base.include Object.const_get(class_name)
-# end
+# Add all helper files to ActionView::Base
+Dir.glob('lib/upserv_foundations/helpers/**/*.rb').each do |file|
+  require file.sub('lib/', '').sub('.rb', '')
+  # Extract the directory path without "lib/"
+  dir_path = File.dirname(file).sub(%r{^lib/}, '')
+  # Convert the directory path to Pascal case without underscores
+  class_name = dir_path.split('/').map do |part|
+    part.split('_').map(&:capitalize).join
+  end.join('::')
+  # Add the final module and class name
+  class_name += "::#{File.basename(file, '.rb').split('_').map(&:capitalize).join}"
+  ActionView::Base.include Object.const_get(class_name)
+end
 
 # Add things you would want in all rails apps
 module UpservFoundations
