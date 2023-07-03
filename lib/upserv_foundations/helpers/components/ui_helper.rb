07624a8fdfe31a3b8baac9ecee93a1e9c35680f7
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-07-03 17:13:07 -0500

    Improve dropdowns

--- lib/upserv_foundations/helpers/components/ui_helper.rb
+++ lib/upserv_foundations/helpers/components/ui_helper.rb
@@ -16,8 +16,11 @@ module UpservFoundations
 
       def dropdown_button(options = {}, &block)
         options[:block] = block
-        # options[:id] is required
-        concat(render('global/components/ui/dropdown_button', options))
+        content_tag 'DIV', id: options[:id],
+                           data: { dropdown_target: 'button',
+                                   action: 'click->dropdown#show' } do
+          block.call
+        end
       end
 
       def dropdown_items(options = {}, &block)
@@ -38,24 +41,23 @@ module UpservFoundations
         options[:block] = block
         options[:text] ||= false
         options[:link] ||= false
-        options[:css_class] = drop_down_css_class(options)
-        link_to_options(options)
-        concat(render('global/components/ui/dropdown_item', options))
-      end
-
-      private
-
-      def link_to_options(options)
-        options[:link_to_options] = { class: drop_down_css_class(options) }
-        options[:link_to_options][:method] = options[:method] if options[:method]
-        options[:link_to_options][:data] = options[:data] if options[:data]
-      end
-
-      def drop_down_css_class(options)
         drop_down_css_class = 'p-2 font-normal block w-full ' \
                               'whitespace-nowrap bg-transparent'
         drop_down_css_class += " #{options[:class]}" if options[:class]
-        drop_down_css_class
+        options[:css_class] = drop_down_css_class
+        options[:link_to_options] = { class: drop_down_css_class }
+        options[:link_to_options][:method] = options[:method] if options[:method]
+        options[:link_to_options][:data] = options[:data] if options[:data]
+
+        content_tag 'LI', class: 'dropdown-item' do
+          if options[:text] & block.blank?
+            link_to(options[:text], options[:link], options[:link_to_options])
+          else
+            content_tag 'DIV', class: options[:css_class] do
+              block.call
+            end
+          end
+        end
       end
     end
   end
--- lib/upserv_foundations/version.rb
+++ lib/upserv_foundations/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module UpservFoundations
-  VERSION = '0.0.48'
+  VERSION = '0.0.49'
 end
