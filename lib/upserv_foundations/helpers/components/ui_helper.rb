75d78ea9d58a57873ca9b7ad3761d2b9b68782b8
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-07-05 13:41:55 -0500

    wip

--- lib/upserv_foundations/helpers/components/ui_helper.rb
+++ lib/upserv_foundations/helpers/components/ui_helper.rb
@@ -5,6 +5,7 @@ module UpservFoundations
     # Set up ui components
     module UiHelper
       def dropdown(options = {}, &block)
+        content_tag 'div', data: { controller: 'testz' }
         content_tag 'div', class: "flex justify-center#{" #{options[:class]}" if options[:class]}",
                            data: { controller: 'dropdown',
                                    action: 'click@window->dropdown#hide touchend@window->dropdown#hide' } do
