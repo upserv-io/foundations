5ddbc5792f08c9669189dd5d1d19642292369595
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-07-05 22:08:00 -0500

    wip

--- lib/upserv_foundations/helpers/components/forms/search_bar_helper.rb
+++ lib/upserv_foundations/helpers/components/forms/search_bar_helper.rb
@@ -12,21 +12,19 @@ module UpservFoundations
           klass += " #{options[:class]}" if options[:class]
           default_style = 'width: 20rem;'
           style = "#{default_style}#{" #{options[:style]}" if options[:style]}"
-          capture do
-            content_tag 'DIV', style: style do
-              # form_with does not accept a style argument %>
-              form_with(scope: :filters, url: method(url_helper).call(*url_helper_args, url_helper_options.merge(filter)), method: :get, class: klass) do |f|
-                filters.each do |key, value|
-                  f.hidden_field(key, value: value)
-                end
-                content_tag 'DIV', class: 'form-search' do
-                  content_tag 'DIV', class: 'form-search-bar' do
-                    f.text_field(:search, value: filters[:search], placeholder: placeholder, class: 'form-control')
-                    link_to('clear', method(url_helper).call(*url_helper_args, url_helper_options.merge(filter(search: nil))), class: 'form-search-clear') if filters[:search].present?
-                  end
-                  f.submit('Search', class: 'btn btn-search')
-                end
+          content_tag 'DIV', style: style do
+            form_with(scope: :filters, url: method(url_helper).call(*url_helper_args, url_helper_options.merge(filter)), method: :get, class: klass) do |f|
+              filters.each do |key, value|
+                concat f.hidden_field(key, value: value)
               end
+
+              concat(content_tag('DIV', class: 'form-search') do
+                concat(content_tag('DIV', class: 'form-search-bar') do
+                  concat(f.text_field(:search, value: filters[:search], placeholder: placeholder, class: 'form-control'))
+                  concat(link_to('clear', method(url_helper).call(*url_helper_args, url_helper_options.merge(filter(search: nil))), class: 'form-search-clear')) if filters[:search].present?
+                end)
+                concat(f.submit('Search', class: 'btn btn-search'))
+              end)
             end
           end
         end
