13c92a239f05910e1830991cd7c033948fce53c7
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-07-07 14:47:53 -0500

    wip

--- lib/upserv_foundations/helpers/components/pages/modal_helper.rb
+++ lib/upserv_foundations/helpers/components/pages/modal_helper.rb
@@ -7,16 +7,16 @@ module UpservFoundations
       module ModalHelper
         def modal(id:, max_width: '600px', min_width: '400px', &block)
           content_tag 'DIV', id: id, class: 'modal', data: { controller: 'modal' } do
-            trigger = content_tag 'DIV', id: "#{id}-trigger", data: { action: 'click->modal#show' }
-            backdrop = content_tag 'DIV', class: 'modal-backdrop', data: { action: 'click->modal#hide' }
+            trigger = content_tag 'DIV', '', id: "#{id}-trigger", data: { action: 'click->modal#show' }
+            backdrop = content_tag 'DIV', '', class: 'modal-backdrop', data: { action: 'click->modal#hide' }
             modal_content_container = content_tag 'DIV', class: 'modal-content-container', style: "max-width: #{max_width}" do
               content_tag 'DIV', class: 'modal-content', style: "min-width: #{min_width}" do
                 modal_close = content_tag 'SPAN', '&times;'.html_safe, class: 'modal-close', data: { action: 'click->modal#hide' }
                 block_called = block.call
-                "#{modal_close}#{block_called})".html_safe
+                (modal_close + block_called).html_safe
               end
             end
-            "#{trigger}#{backdrop}#{modal_content_container}".html_safe
+            (trigger + backdrop + modal_content_container).html_safe
           end
         end
       end
