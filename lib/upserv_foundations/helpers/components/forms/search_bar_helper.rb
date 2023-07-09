bb975f22b29df7f1a2980c9a7b4544847ebafa7b
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-07-08 21:28:03 -0500

    wip

--- lib/upserv_foundations/helpers/components/forms/search_bar_helper.rb
+++ lib/upserv_foundations/helpers/components/forms/search_bar_helper.rb
@@ -14,11 +14,13 @@ module UpservFoundations
           style = "#{default_style}#{" #{options[:style]}" if options[:style]}"
           content_tag 'DIV', style: style do
             form_with(scope: :filters, url: method(url_helper).call(*url_helper_args, url_helper_options.merge(filter)), method: :get, class: klass) do |f|
+              filters_array = []
               filters.each do |key, value|
-                concat f.hidden_field(key, value: value)
+                filters_array << f.hidden_field(key, value: value)
               end
+              filters_html = filters_array.join
 
-              content_tag 'DIV', class: 'form-search' do
+              entire_search_bar = content_tag 'DIV', class: 'form-search' do
                 search_input_container = content_tag 'DIV', class: 'form-search-bar' do
                   text_input = f.text_field :search, value: filters[:search], placeholder: placeholder, class: 'form-control'
                   clear_link = link_to 'clear', method(url_helper).call(*url_helper_args, url_helper_options.merge(filter(search: nil))), class: 'form-search-clear' if filters[:search].present?
@@ -27,6 +29,7 @@ module UpservFoundations
                 submit_button = f.submit 'Search', class: 'btn btn-search'
                 (search_input_container + submit_button).html_safe
               end
+              (filters_html + entire_search_bar).html_safe
             end
           end
         end
--- lib/upserv_foundations/version.rb
+++ lib/upserv_foundations/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module UpservFoundations
-  VERSION = '0.0.0.79'
+  VERSION = '0.0.0.80'
 end
