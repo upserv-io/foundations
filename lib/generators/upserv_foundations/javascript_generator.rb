904904ae616713eb0fcff1e84e7565a574ed8e27
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-07-10 11:28:53 -0500

    wip

--- lib/generators/upserv_foundations/javascript_generator.rb
+++ lib/generators/upserv_foundations/javascript_generator.rb
@@ -4,14 +4,15 @@ module UpservFoundations
   module Generators
     # test run
     class JavascriptGenerator < Rails::Generators::Base
-      # include Rails::Generators::ResourceHelpers
-
       source_root File.expand_path('templates', __dir__)
 
-      # argument :name, type: :string, desc: 'Name of the controller'
-
       def copy_js_files
-        template 'my_controller.js', 'app/javascript/controllers/my_controller.js'
+        template 'dropdown_controller.js',
+                 'app/javascript/controllers/dropdown_controller.js'
+        template 'modal_controller.js',
+                 'app/javascript/controllers/modal_controller.js'
+        template 'search_select_multiple_controller.js',
+                 'app/javascript/controllers/search_select_multiple_controller.js'
       end
     end
   end
