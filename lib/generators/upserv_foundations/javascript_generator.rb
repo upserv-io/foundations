865d377cbedf2531c5eaf54d92011c9c22aa006a
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-07-10 11:08:23 -0500

    wip

--- lib/generators/upserv_foundations/javascript_generator.rb
+++ lib/generators/upserv_foundations/javascript_generator.rb
@@ -4,7 +4,9 @@ module UpservFoundations
   module Generators
     # test run
     class JavascriptGenerator < Rails::Generators::Base
-      source_root File.expand_path('templates', __dir__)
+      include Rails::Generators::ResourceHelpers
+
+      source_root File.expand_path('../templates', __File__)
 
       def copy_js_files
         template 'my_controller.js', 'app/javascript/controllers/my_controller.js'
--- lib/upserv_foundations/version.rb
+++ lib/upserv_foundations/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module UpservFoundations
-  VERSION = '0.0.0.90'
+  VERSION = '0.0.0.91'
 end
