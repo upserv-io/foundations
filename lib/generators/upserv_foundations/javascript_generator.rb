c6ec3825faa83b7a8da10d42b7ed000b8a03ca59
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-07-10 11:23:50 -0500

    wip

--- lib/generators/upserv_foundations/javascript_generator.rb
+++ lib/generators/upserv_foundations/javascript_generator.rb
@@ -8,7 +8,7 @@ module UpservFoundations
 
       source_root File.expand_path('templates', __dir__)
 
-      argument :name, type: :string, desc: 'Name of the controller'
+      # argument :name, type: :string, desc: 'Name of the controller'
 
       def copy_js_files
         template 'my_controller.js', 'app/javascript/controllers/my_controller.js'
