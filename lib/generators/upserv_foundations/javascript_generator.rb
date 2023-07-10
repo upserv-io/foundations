de32d6529357a7c37437b2c67778ddf3e62e683d
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-07-10 11:22:12 -0500

    wip

--- lib/generators/upserv_foundations/javascript_generator.rb
+++ lib/generators/upserv_foundations/javascript_generator.rb
@@ -4,9 +4,11 @@ module UpservFoundations
   module Generators
     # test run
     class JavascriptGenerator < Rails::Generators::Base
-      include Rails::Generators::ResourceHelpers
+      # include Rails::Generators::ResourceHelpers
 
       source_root File.expand_path('templates', __dir__)
+
+      argument :name, type: :string, desc: 'Name of the controller'
 
       def copy_js_files
         template 'my_controller.js', 'app/javascript/controllers/my_controller.js'
