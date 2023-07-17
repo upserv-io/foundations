556b0196a79d20e45139a6eac5af9aff4f02309c
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-07-17 17:52:29 -0500

    wip

--- lib/upserv_foundations/helpers/components/pages/tabs_helper.rb
+++ lib/upserv_foundations/helpers/components/pages/tabs_helper.rb
@@ -5,31 +5,6 @@ module UpservFoundations
     module Pages
       # tabs (ie top nav)
       module TabsHelper
-        def tabs(options = {}, &block)
-          options[:class] = "tabs#{" #{options[:class]}" if options[:class]}"
-          content_tag 'DIV', options do
-            block.call
-          end
-        end
-
-        def tab_item(options = {}, &block)
-          options[:class] = "tab-item#{" #{options[:class]}" if options[:class]}"
-          text = options.delete(:text)
-          url = options.delete(:url)
-          icon = options.delete(:icon)
-          icon_class = icon ? "tab-item-icon fa-solid #{icon}" : nil
-          if text && url && block.blank?
-            link_to url, options do
-              icon_elm = icon_class ? content_tag('I', nil, class: icon_class) : ''
-              display = content_tag 'SPAN', text
-              (icon_elm + display).html_safe
-            end
-          else
-            content_tag 'DIV', options do
-              block.call
-            end
-          end
-        end
       end
     end
   end
