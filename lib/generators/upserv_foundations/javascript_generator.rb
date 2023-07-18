0328a37f54de6a113bda345424eb0c66e94f94f1
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-07-17 19:19:36 -0500

    wip

--- lib/generators/upserv_foundations/javascript_generator.rb
+++ lib/generators/upserv_foundations/javascript_generator.rb
@@ -7,6 +7,8 @@ module UpservFoundations
       source_root File.expand_path('templates', __dir__)
 
       def copy_js_files
+        template 'form/dropdown_controller.js',
+                 'app/javascript/controllers/form/dropdown_controller.js'
         template 'dropdown_controller.js',
                  'app/javascript/controllers/dropdown_controller.js'
         template 'modal_controller.js',
