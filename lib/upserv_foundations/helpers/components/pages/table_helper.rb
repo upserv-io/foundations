f4e7ca48350f23c068984ed8f45afaa3b3a3496b
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-08-07 13:49:16 -0500

    wip

--- lib/upserv_foundations/helpers/components/pages/table_helper.rb
+++ lib/upserv_foundations/helpers/components/pages/table_helper.rb
@@ -21,8 +21,8 @@ module UpservFoundations
 
         def table_body(enum, options = {}, &block)
           content_tag 'TBODY', options do
-            enum&.each do |item|
-              block.call(item)
+            enum&.each_with_index do |item, index|
+              block.call(item, index)
             end
           end
         end
