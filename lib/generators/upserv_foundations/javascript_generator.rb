a30df1ea026f617a9225d4eb0a36cd65c37b5f0f
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-08-09 15:47:40 -0500

    wip

--- lib/generators/upserv_foundations/javascript_generator.rb
+++ lib/generators/upserv_foundations/javascript_generator.rb
@@ -11,14 +11,20 @@ module UpservFoundations
                  'app/javascript/controllers/form/select_display_toggle_controller.js'
         template 'form/text_area_controller.js',
                  'app/javascript/controllers/form/dropdown_controller.js'
+
         template 'pages/page_body_columns_controller.js',
                  'app/javascript/controllers/pages/page_body_columns_controller.js'
+
         template 'ui/dropdown_controller.js',
                  'app/javascript/controllers/ui/dropdown_controller.js'
         template 'ui/tooltip_controller.js',
                  'app/javascript/controllers/ui/tooltip_controller.js'
+
         template 'modal_controller.js',
                  'app/javascript/controllers/modal_controller.js'
+
+        template 'radio_display_toggle_controller.js',
+                 'app/javascript/controllers/radio_display_toggle_controller.js'
         template 'search_select_controller.js',
                  'app/javascript/controllers/search_select_controller.js'
         template 'search_select_multiple_controller.js',
