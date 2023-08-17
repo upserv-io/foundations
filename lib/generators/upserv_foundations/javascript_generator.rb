c1e6070e4222c2213543ff83dbfdc6c3fa68a481
Clark Taylor <clarktaylor@Clarks-MacBook-Pro.local>
2023-08-17 15:15:22 -0500

    wip

--- lib/generators/upserv_foundations/javascript_generator.rb
+++ lib/generators/upserv_foundations/javascript_generator.rb
@@ -7,30 +7,41 @@ module UpservFoundations
       source_root File.expand_path('templates', __dir__)
 
       def copy_js_files
+        # stimulus controllers
+        # NOTE: these should all be moved to controllers/... here in UF
+        # ex form/checkbox_display_toggle_controller.js =>
+        #    controllers/form/checkbox_display_toggle_controller.js
+        # they are good in the rails app though
+
+        # form
         template 'form/checkbox_display_toggle_controller.js',
                  'app/javascript/controllers/form/checkbox_display_toggle_controller.js'
+        template 'form/radio_display_toggle_controller.js',
+                 'app/javascript/controllers/form/radio_display_toggle_controller.js'
         template 'form/select_display_toggle_controller.js',
                  'app/javascript/controllers/form/select_display_toggle_controller.js'
         template 'form/text_area_controller.js',
                  'app/javascript/controllers/form/dropdown_controller.js'
 
+        # pages
         template 'pages/page_body_columns_controller.js',
                  'app/javascript/controllers/pages/page_body_columns_controller.js'
 
+        # ui
         template 'ui/dropdown_controller.js',
                  'app/javascript/controllers/ui/dropdown_controller.js'
         template 'ui/tooltip_controller.js',
                  'app/javascript/controllers/ui/tooltip_controller.js'
 
+        # un-namespaced (the select controllers should be in the form namespace...)
         template 'modal_controller.js',
                  'app/javascript/controllers/modal_controller.js'
-
-        template 'radio_display_toggle_controller.js',
-                 'app/javascript/controllers/radio_display_toggle_controller.js'
         template 'search_select_controller.js',
                  'app/javascript/controllers/search_select_controller.js'
         template 'search_select_multiple_controller.js',
                  'app/javascript/controllers/search_select_multiple_controller.js'
+
+        # global js
         template 'global/check_and_set_timezone_cookie.js',
                  'app/javascript/global/check_and_set_timezone_cookie.js'
         template 'global/dropdown.js',
