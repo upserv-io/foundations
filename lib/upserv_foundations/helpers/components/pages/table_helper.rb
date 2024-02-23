a2c45e541e443c61f8ad017bd071c885458f6fd5
Clark Taylor <clarktaylor@Clarks-MBP.fios-router.home>
2024-02-23 12:44:19 -0600

    wip

--- lib/upserv_foundations/helpers/components/pages/table_helper.rb
+++ lib/upserv_foundations/helpers/components/pages/table_helper.rb
@@ -6,6 +6,10 @@ module UpservFoundations
       # tables
       module TableHelper
         def table(options = {}, &block)
+          # sole reason for giving name uf-table is because tailwind defaults to
+          # border-collapse: collapse; which is not what we want because our table
+          # header is sticky... and when you have a sticky table header with border
+          # collapse, the table header border is not visible when you scroll
           options[:class] = "uf-table #{options[:class]}".strip
           content_tag 'TABLE', options do
             block.call
